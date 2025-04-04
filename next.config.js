d774fc49f56932280bfd0b36f0c95db0b00a84a2
Parth <parth.kale.dev@gmail.com>
2025-04-04 10:09:54 +0530

    Updated 3

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+
 }
 
 module.exports = nextConfig
